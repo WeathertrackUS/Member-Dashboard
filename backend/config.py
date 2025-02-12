ccd094b7bb82dd05fb9a1f57571bd168c57a321a
WeatherboySuper <SuperPlays1020@gmail.com>
2025-02-12 12:23:52 -0600

    Create sqlalchemy config

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,6 @@
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
@@ -6,3 +8,10 @@ class Config:
     DEBUG = os.environ.get('DEBUG', 'False') == 'True'
     ALLOWED_EXTENSIONS = {'psd', 'jpg', 'jpeg', 'png', 'gif'}
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), 'uploads')
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def init_db(app):
+    db.init_app(app)
+    migrate.init_app(app, db)
